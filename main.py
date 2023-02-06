ed081ab3a5125bcce092c8726f99f721e366d97f
unknown <ktn.andrea@gmail.com>
2023-02-06 19:13:10 +0100

    Added logging and timeit

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ from scraper import ImdbScraper
 from rating_calculator import RatingCalculator
 from movie import Movie
 import codecs, json
+import logging, timeit
 
 def main():
 
+    start = timeit.default_timer()
+    logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.INFO)
+    
+    logging.info('Web scraping started...')
     Imdb_scraper = ImdbScraper()
     uri_top_250 = "https://www.imdb.com/chart/top/"
     Imdb_Top_20 = Imdb_scraper.get_top_20(uri_top_250)
 
+    logging.info('Adjusting ratings...')
     Movie_Oscar_Calculator = RatingCalculator()
     Movie_Oscar_Calculator.penalize_movies(Imdb_Top_20)
     Movie_Oscar_Calculator.oscar_calculator(Imdb_Top_20)
 
+    logging.info('Writing results to results.json...')
     with codecs.open("results.json", "w", encoding='utf-8') as f:
         for movie in Imdb_Top_20:
             f.write(json.dumps(Movie.to_json(movie), indent=4, separators=(',', ': '), ensure_ascii=False))
-    
+
+    end = timeit.default_timer()
+    logging.info('Finished in: {} seconds.'.format(end-start))
 
     
 
--- rating_calculator.py
+++ rating_calculator.py
@@ -7,14 +7,11 @@ class RatingCalculator():
     def penalize_movies(movies):
         max_num_of_ratings = max(movie.num_of_ratings for movie in movies)
         for movie in movies:
-            print(movie.rating)
             deviation = max_num_of_ratings - movie.num_of_ratings
             deduction = (deviation // 100000) * 0.1
             new_rating = round(movie.rating - deduction, 1)
             movie.rating_penalized = new_rating
             movie.rating_adjusted = new_rating
-            print(movie.rating_penalized)
-            print('*' * 20)
 
     @staticmethod
     def oscar_calculator(movies):
