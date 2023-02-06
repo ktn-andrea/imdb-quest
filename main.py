3de51d9fce3a16fc4bdc954f19e63e27d66931ec
unknown <ktn.andrea@gmail.com>
2023-02-06 16:13:59 +0100

    Created Movie class and scraped 4 parameters for each movie

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from movie import Movie
 
 
 
@@ -16,11 +17,30 @@ def main():
     
     Imdb_Top_20 = []
 
-    for title_item in titles_top_20:
+    for title_item, rating_item in zip(titles_top_20, ratings_top_20):
 
         title = title_item.findChild('a').get_text()
+        ratings = rating_item.findChild('strong')['title']
+        rating = float(ratings.split()[0])
+        num_of_ratings = int(ratings.split()[3].replace(',', '')) 
+        movie_uri = "https://imdb.com" + str(title_item.findChild('a', href=True)['href'])
 
-        print(title)
+        req_movie = Request(movie_uri , headers={'User-Agent': 'Mozilla/5.0'})
+        movie_soup = BeautifulSoup(urlopen(req_movie).read(), "html.parser")
+
+        awards = movie_soup.find('div', attrs={"data-testid": "awards"})
+        oscars = awards.findChild('a', attrs={"role" : "button", "class": "ipc-metadata-list-item__label ipc-metadata-list-item__label--link"}).get_text()
+        oscar_wins = int(oscars.split()[1]) if "Won" in oscars and "Oscar" in oscars else 0
+
+        movie = Movie(title, rating, num_of_ratings, oscar_wins)
+
+        Imdb_Top_20.append(movie)
+
+    for i in Imdb_Top_20:
+        print(i.title)
+        print(i.num_of_oscar_wins)
+        print(i.rating)
+        print(i.num_of_ratings)
         
 
 
